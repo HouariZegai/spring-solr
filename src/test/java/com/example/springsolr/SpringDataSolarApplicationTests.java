ddba4708cfe223f83fae3f1f047705b2087a0b73
HouariZegai <houarizegai14@gmail.com>
2022-03-24 21:21:47 +0100

    feat: init project

---
+++ src/test/java/com/example/springsolr/SpringDataSolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsolr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataSolarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
